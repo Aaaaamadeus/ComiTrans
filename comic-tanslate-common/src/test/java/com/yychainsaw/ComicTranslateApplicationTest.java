96c083ef7d8d612904b33ebbf4da5b0e075f70a5
YYchainsAw <2560634677@qq.com>
2025-12-15 18:39:50 +0800

    Springboot后端初始化 第一次提交

---
+++ comic-tanslate-common/src/test/java/com/yychainsaw/ComicTranslateApplicationTest.java
@@ -0,0 +1,38 @@
+package com.yychainsaw;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ComicTranslateApplicationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ComicTranslateApplicationTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ComicTranslateApplicationTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
