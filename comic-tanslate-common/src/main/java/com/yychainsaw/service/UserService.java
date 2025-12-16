3eb31e03bf84cf8295195fcf4b94d7d707144806
YYchainsAw <2560634677@qq.com>
2025-12-16 08:23:31 +0800

    User服务框架搭建

---
+++ comic-tanslate-common/src/main/java/com/yychainsaw/service/UserService.java
@@ -0,0 +1,4 @@
+package com.yychainsaw.service;
+
+public interface UserService {
+}
---
+++ comic-tanslate-common/src/main/java/com/yychainsaw/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.yychainsaw.service.impl;
+
+import com.yychainsaw.mapper.UserMapper;
+import com.yychainsaw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+}
